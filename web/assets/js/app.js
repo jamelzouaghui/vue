aa6efce4941406a03fa51aa91bdfece111e97108
Ahmed <ahmed.souilem.ing@gmail.com>
2019-03-29 17:07:23 +0100

    create first compoent home

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -2,7 +2,9 @@ import Vue from 'vue';
 import App from './components/app.vue';
 import Home from './components/home.vue';
 
+global.jQuery = require('jquery');
 global.axios = require('axios');
+require('bootstrap-sass');
 
 
 var app = new Vue({
